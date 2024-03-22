1329a22ec434a6761ce8f25359f590dde158b983
Marcdaus <146002562+Marcdaus@users.noreply.github.com>
2024-03-22 13:07:48 +0100

    Add files via upload

---
+++ proyecte/scripts/prova.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+marc() {
+sudo apt update &
+}
+marc
+
+
+
+ps aux | grep "&"
+(
+sleep 1
+echo "10"
+echo "# Paso 1"; sleep 1
+if apt update
+then
+echo "hola"
+fi
+echo "20"
+echo "# Paso 2"; sleep 1
+echo "30"
+echo "# Paso 3"; sleep 1
+echo "50"
+
+echo "# Paso 4"; sleep 1
+echo "70"
+echo "# Paso 5"; sleep 1
+echo "90"
+echo "# Terminado"
+echo "100"
+)|
+zenity --progress \
+  --title="https://atareao.es" \
+  --text="Iniciando el proceso" \
+  --percentage=0
+
+ps aux | grep "&"
